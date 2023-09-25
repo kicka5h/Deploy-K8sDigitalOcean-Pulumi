7957a6708df516b02d3a58836106234684659aaa
akarczag <ashley.karczag@eradigm.com>
2023-09-25 15:43:38 -0700

    fix config

--- python/__main__.py
+++ python/__main__.py
@@ -1,23 +1,23 @@
 """A Python Pulumi program"""
 
 import pulumi
-from pulumi_digitalocean import kubernetes
+from pulumi_digitalocean import kubernetes_cluster
+from pulumi_digitalocean import Provider
 
 # Define the DO token.
 do_token = pulumi.Config("do_token")
 
+do_provider=Provider("do-provider", token="YOUR_DIGITALOCEAN_TOKEN")
+
 # Create a new Digital Ocean Kubernetes cluster in the sfo2 region.
-do_cluster = kubernetes.Cluster("doCluster",
+do_cluster = kubernetes_cluster.KubernetesCluster("doCluster",
     region="sfo2",
     version="1.20.2-do.0",
     node_pool={
         "name": "worker-pool",
         "size": "s-2vcpu-2gb",
         "nodeCount": 3
-    },
-    opts=pulumi.ResourceOptions(
-        environs={ "DIGITALOCEAN_TOKEN": do_token }
-    )
+    }
 )
 
 # Export the cluster's endpoint and Kubeconfig.
