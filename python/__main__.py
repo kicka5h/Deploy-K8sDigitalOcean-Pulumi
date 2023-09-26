fb3292c4b7dc89fc6ad23e11b2d2a181a5de449b
akarczag <ashley.karczag@eradigm.com>
2023-09-26 10:13:13 -0700

    update

---
+++ python/__main__.py
@@ -0,0 +1,21 @@
+"""A Python Pulumi program"""
+
+import pulumi
+from pulumi_digitalocean import kubernetes_cluster
+from pulumi_digitalocean import Provider
+
+# Create a new Digital Ocean Kubernetes cluster in the sfo2 region.
+do_cluster = kubernetes_cluster.KubernetesCluster("doCluster",
+    name="galaxy-cluster",                                              
+    region="sfo2",
+    version="latest",
+    node_pool={
+        "name": "singe-worthy",
+        "size": "s-2vcpu-2gb",
+        "nodeCount": 3
+    }
+)
+
+# Export the cluster's endpoint and Kubeconfig.
+pulumi.export("clusterEndpoint", do_cluster.endpoint)
+pulumi.export("clusterKubeconfig", do_cluster.kube_configs[0]["rawConfig"])
